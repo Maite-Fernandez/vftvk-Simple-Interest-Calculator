2ee8d18d764163d89158b3cffe2649dd0ef18e6e
Maite Fernandez <maifermat.1997@gmail.com>
2021-02-05 23:06:05 +0000

    Validators

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ function compute()
     amount = Number(document.getElementById("amount").value);
     interest = Number(document.getElementById("rate").value);
     years = Number(document.getElementById("years").value);
+    //Check if fields are empty
+          if(amount.value<=0){
+                alert("Please enter a positive amount");
+                amount.focus();
+                return false;
+          }
+          if(years.value<=0){
+                alert("Please enter a positive number of years");
+                years.focus();
+                return false;
+          }
     var result = amount*(interest/100)*years;
     var year = 2020 + years;
     text = "If you deposit " + amount + ",<br>" + "at an interest rate of " + interest + "%."+ "<br>" + "You will receive an amount of " + Math.round(result) + ",<br>" + "in the year " +  year;
     document.getElementById("result").innerHTML = text;
+    return true;
         
 }
         
