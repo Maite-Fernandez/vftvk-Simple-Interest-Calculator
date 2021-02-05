b3297248239b37e05c697c5f97bae659ab92d2c2
Maite Fernandez <maifermat.1997@gmail.com>
2021-02-05 22:15:44 +0000

    Interest

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    amount = document.getElementById("amount").value;
+    interest = document.getElementById("interest").value;
+    years = document.getElementById("years").value;
+    var result = amount;
+    for (var i = 0; i < years; i++) {
+        result+=result*(interest/100);
+    }
+    text = "If you deposit " + amount + ",<br>" + "at an interest rate of " + rate + "%."+ "<br>" + "You will receive an amount of " + result + ",<br>" + "in the year" +  years+2021;
+    document.getElementById("result").innerHTML = text;
+        
 }
         
